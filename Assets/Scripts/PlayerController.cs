1fa4ad0b9c79cff144c4207ecefbb74c8ea647a3
EricaAV <a59632@alunos.ipb.pt>
2025-03-06 22:27:06 +0000

    Atualização
    - Corrigi alguns erros que me deu durante o dia;
    - Adicionei Binding;
    - Decorei o mapa;
    - Erro: o boneco não anda com o código certo.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
+    public InputAction MoveAction;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MoveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = 0.0f;
-
-        if (Keyboard.current.leftArrowKey.isPressed)
-        {
-            horizontal = -1.0f;
-        }
-        else if (Keyboard.current.rightArrowKey.isPressed)
-        {
-            horizontal = 1.0f;
-        }
-
-        Debug.Log(horizontal);
-
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        Vector2 move = MoveAction.ReadValue<Vector2>();
+        Debug.Log(move);
+        Vector2 position = (Vector2)transform.position + move * 3.0f * Time.deltaTime;
         transform.position = position;
     }
+
+
 }
