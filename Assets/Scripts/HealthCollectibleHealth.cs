30f50a06a07edb1c4d859e1a5d087eb2ac433f8b
EricaAV <a59632@alunos.ipb.pt>
2025-03-10 17:49:44 +0000

    Atualizações
    -Adicionei áudio ao jogo

--- Assets/Scripts/HealthCollectibleHealth.cs
+++ Assets/Scripts/HealthCollectibleHealth.cs
@@ -2,22 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class HealthCollectible : MonoBehaviour
 {
-
+    public AudioClip collectedClip; // Adicionando o AudioClip
 
     void OnTriggerEnter2D(Collider2D other)
     {
         PlayerController controller = other.GetComponent<PlayerController>();
 
-
         if (controller != null && controller.health < controller.maxHealth)
         {
             controller.ChangeHealth(1);
+            controller.PlaySound(collectedClip); // Chamando PlaySound para tocar o som
             Destroy(gameObject);
         }
     }
 }
+
 
 
